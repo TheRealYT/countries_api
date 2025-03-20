0400043724489fd48e7cc19b248a01df7bed8b6c
TheRealYT <85513618+TheRealYT@users.noreply.github.com>
2025-03-21 00:17:36 +0300

    handle the animation end

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,18 @@ import Header from '@/components/Header.jsx';
 import Footer from '@/components/Footer.jsx';
 
 import './App.css';
+import {useRef, useState} from 'react';
 
 function App() {
   const client = new QueryClient();
+  const globe = useRef({});
+  const [show, setShow] = useState(false);
 
   return (
     <QueryClientProvider client={client}>
-      <Header/>
-      <WorldView/>
-      <Footer/>
+      <Header show={show}/>
+      <WorldView ref={globe} onAnimationEnd={() => setShow(true)}/>
+      <Footer show={show}/>
     </QueryClientProvider>
   );
 }
