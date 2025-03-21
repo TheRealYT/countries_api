6bfff7b841989ed4114d65061ae8a3778ed8a6d1
TheRealYT <85513618+TheRealYT@users.noreply.github.com>
2025-03-21 17:17:06 +0300

    create api

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import {useRef, useState} from 'react';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 import WorldView from './components/WorldView.jsx';
@@ -5,18 +6,29 @@ import Header from '@/components/Header.jsx';
 import Footer from '@/components/Footer.jsx';
 
 import './App.css';
-import {useRef, useState} from 'react';
 
 function App() {
   const client = new QueryClient();
   const globe = useRef({});
   const [show, setShow] = useState(false);
 
+  const api = {
+    toggleUi() {
+      setShow(!show);
+    },
+    search: globe.current.search,
+    select: globe.current.select
+  };
+
+  const onAnimationEnd = () => {
+    setTimeout(() => setShow(true), 300);
+  };
+
   return (
     <QueryClientProvider client={client}>
-      <Header show={show}/>
-      <WorldView ref={globe} onAnimationEnd={() => setShow(true)}/>
-      <Footer show={show}/>
+      <Header show={show} api={api}/>
+      <WorldView ref={globe} onAnimationEnd={onAnimationEnd}/>
+      <Footer show={show} api={api}/>
     </QueryClientProvider>
   );
 }
