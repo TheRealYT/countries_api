198759b7c90296636811b9bea18e0adac03adc43
TheRealYT <85513618+TheRealYT@users.noreply.github.com>
2025-03-21 17:16:57 +0300

    add action buttons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,24 @@
-import {cn} from '@/lib/utils.js';
+import {LocateIcon, Maximize, Minimize} from 'lucide-react';
 
-export default function Footer({show = true}) {
+import {cn} from '@/lib/utils.js';
+import {Button} from '@/components/ui/button.jsx';
+
+export default function Footer({show = true, api}) {
   return (
     <>
-      <footer className={cn('bg-secondary transition-[bottom] duration-300 absolute h-12 w-full z-10 left-0', show ? 'bottom-0' : '-bottom-12')}>
+      <Button size="icon" onClick={api.toggleUi}
+              className={cn('z-20 absolute bottom-4 right-8 duration-500', show ? '' : 'hover:bottom-0 hover:right-0 -bottom-6 -right-6')}>{show ?
+        <Maximize/> : <Minimize/>}</Button>
+
+      <footer
+        className={cn('flex gap-2 px-8 py-4 transition-[bottom] duration-500 absolute w-full z-10 left-0', show ? '-bottom-0' : '-bottom-20')}>
+
+        <div className="ml-auto"/>
+
+        <Button size="icon"><LocateIcon/></Button>
+
+        <div className="w-9"/>
+
       </footer>
     </>);
 }
