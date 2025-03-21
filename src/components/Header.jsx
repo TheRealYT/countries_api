16edb929ba08e14f82edf689712dcb1e2c313299
TheRealYT <85513618+TheRealYT@users.noreply.github.com>
2025-03-21 17:16:49 +0300

    add search box

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import {cn} from '@/lib/utils.js';
 
-export default function Header({show = true}) {
+import Search from '@/components/Search.jsx';
+
+export default function Header({show = true, api}) {
   return (
-    <header className={cn('bg-secondary absolute transition-[top] duration-300 w-full h-12 z-10 left-0', show ? 'top-0' : '-top-12')}>
-      <nav>
+    <header
+      className={cn('bg-secondary absolute transition-[top] duration-500 w-full h-12 z-10 left-0', show ? 'top-0' : '-top-12')}>
+      <nav className="flex items-center h-full px-8 group" hidden={!show}>
+
+        <div className="ml-auto"/>
+
+        <Search api={api}/>
+
       </nav>
     </header>
   );
